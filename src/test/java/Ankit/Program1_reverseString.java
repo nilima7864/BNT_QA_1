b273dcab89d799be114c59ffb52f54165813885d
Ankit Dhakulkar <ankit.dhakulkar@bnt.local>
2022-07-08 17:21:14 +0530

    first code

---
+++ src/test/java/Ankit/Program1_reverseString.java
@@ -0,0 +1,31 @@
+package Ankit;
+
+public class Program1_reverseString {
+
+	public static void main(String[] args) {
+
+		System.out.println("reverse string normal");
+		String s = "Today is the best day";
+		String rev = "";
+
+		for (int i = s.length() - 1; i >= 0; i--) {
+			char ch = s.charAt(i);
+			rev = rev + ch;
+
+		}
+		System.out.println(rev);
+
+		System.out.println("===============================");
+
+		System.out.println("reverse string with stringbuffer");
+
+		StringBuffer sb = new StringBuffer("java");
+		System.out.println(sb);
+		sb.reverse();
+		System.out.println("after reverse " + sb);
+
+		System.out.println("===============================");
+
+	}
+
+}
