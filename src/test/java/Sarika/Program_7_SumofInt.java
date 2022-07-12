41cebcebd9912aa5e4ba7cb7ff2fe12799513230
Sarika.nazare <nrsarika10@gmail.com>
2022-07-12 17:59:23 +0530

    Sarika remaining programs2

---
+++ src/test/java/Sarika/Program_7_SumofInt.java
@@ -0,0 +1,15 @@
+package Sarika;
+
+import java.util.Scanner;
+
+public class Program_7_SumofInt {
+	   public static void main(String[] args) {  
+		      System.out.println("Enter the Two numbers for addtion: ");  
+		      Scanner readInput = new Scanner(System.in);  
+		      int a = readInput.nextInt();  
+		        int b = readInput.nextInt();  
+		        readInput.close();  
+		        System.out.println("The sum of a and b is = " + Integer.sum(a, b));  
+		    }  
+		}  
+
