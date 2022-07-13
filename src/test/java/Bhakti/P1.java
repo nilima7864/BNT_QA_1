1c3ff622dfc2141f1849506ea1673cf9783570f0
bhaktibk <bskakade@gmail.com>
2022-07-13 11:57:26 +0530

    New peogram line added in p1 class

--- src/test/java/Bhakti/P1.java
+++ src/test/java/Bhakti/P1.java
@@ -3,7 +3,10 @@ package Bhakti;
 public class P1 {
 
 	public static void main(String[] args) {
-		System.out.println("This is 1st change");
-		System.out.println("This is 1st change");
-	}
+		 
+				System.out.println("This is 1st change");
+				System.out.println("secind line added");
+			
+		}
+	
 }
