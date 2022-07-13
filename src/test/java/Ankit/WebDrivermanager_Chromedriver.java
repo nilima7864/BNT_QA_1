3667031732246007ef3bf0fbc0f64304b1041a9c
Ankit Dhakulkar <ankit.dhakulkar@bnt.local>
2022-07-13 13:22:39 +0530

    Ankit_Just_RenameTheClassName

--- src/test/java/Ankit/WebDrivermanager_Chromedriver.java
+++ src/test/java/Ankit/WebDrivermanager_Chromedriver.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 
-public class WebDrivermanager {
+public class WebDrivermanager_Chromedriver {
 	public static void main(String[] args) {
 		
 		WebDriverManager.chromedriver().setup();
