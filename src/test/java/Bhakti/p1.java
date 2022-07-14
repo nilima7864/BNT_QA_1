feddf8c0064fc2d0c8b82e0d83aa0e46c34eba72
Sarika.nazare <nrsarika10@gmail.com>
2022-07-14 12:29:21 +0530

    Added one p1 class

--- src/test/java/Bhakti/p1.java
+++ src/test/java/Bhakti/p1.java
@@ -1,8 +1,9 @@
 package Bhakti;
 
-public class p1 {
-	public static void main(String[] args) {
-		System.out.println("b2");
-	}
+public class P1 {
 
+	public static void main(String[] args) {
+				System.out.println("This is 1st change");
+				System.out.println("secind line added");
+		}
 }
