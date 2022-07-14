6b599b85e5b340e643b7140e6a797d61e06f1596
Sarika.nazare <nrsarika10@gmail.com>
2022-07-14 13:19:38 +0530

    Conflict2

---
+++ src/test/java/Sarika/Conflict2.java
@@ -0,0 +1,11 @@
+package Sarika;
+
+public class Conflict2 {
+public static void main(String[] args) {
+	System.out.println("Hieee");
+    System.out.println("Bye");
+    System.out.println("Hie");
+    System.out.println("SArika");
+    System.out.println("SArika");
+}
+}
