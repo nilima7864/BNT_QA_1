0a68b131107c557485e9a75212e8f3c542a9054e
Sarika.nazare <nrsarika10@gmail.com>
2022-07-14 18:11:58 +0530

    resolved one conflict

--- src/test/java/Sarika/Conflict01.java
+++ src/test/java/Sarika/Conflict01.java
@@ -6,6 +6,6 @@ public static void main(String[] args) {
     System.out.println("Bye");
     System.out.println("Hie");
     System.out.println("SArika");
-    System.out.println("SArika");
+ 
 }
 }
