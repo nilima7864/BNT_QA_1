a7799e0ab29a628de076f8d5304d503a3a7fbff7
Sarika.nazare <nrsarika10@gmail.com>
2022-07-14 13:03:15 +0530

    remove line

--- src/test/java/Sarika/conflict1.java
+++ src/test/java/Sarika/conflict1.java
@@ -2,10 +2,13 @@ package Sarika;
 
 public class conflict1 {
 public static void main(String[] args) {
-	System.out.println("Hieee");
-	System.out.println("Bye");
-	System.out.println("Hie");
-	System.out.println("by");
+	   System.out.println("Hieee");
+	    System.out.println("Bye");
+	    System.out.println("Hie");
+	    System.out.println("SArika");
+	    
+	    
+	    
 	
 }
 
