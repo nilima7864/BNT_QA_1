f8849743676495173c372cbf2c7b919e83f3becf
Sarika.nazare <nrsarika10@gmail.com>
2022-07-14 15:48:21 +0530

    Pushed2

--- src/test/java/Sarika/Conflict01.java
+++ src/test/java/Sarika/Conflict01.java
@@ -2,7 +2,7 @@ package Sarika;
 
 public class Conflict01 {
 public static void main(String[] args) {
-	System.out.println("Hieee");
+	    System.out.println("Hieee");
     System.out.println("Bye");
     System.out.println("Hie");
     System.out.println("SArika");
