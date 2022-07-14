839822791946ceb52e206148bb8e94e9c6e643e8
Sarika.nazare <nrsarika10@gmail.com>
2022-07-14 18:09:20 +0530

    Conflict resolved

--- src/test/java/Sarika/Conflict01.java
+++ src/test/java/Sarika/Conflict01.java
@@ -2,17 +2,10 @@ package Sarika;
 
 public class Conflict01 {
 public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("Hieee");
     System.out.println("Bye");
     System.out.println("Hie");
     System.out.println("SArika");
-=======
-	    System.out.println("Hieee");
-    System.out.println("Bye");
-    System.out.println("Hie");
-    System.out.println("SArika");
-    System.out.println("SArika");
->>>>>>> NewConflict
+
 }
 }
