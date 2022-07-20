b0662d88b19ac38ac2ce67c69dfa7b3e540dfcc3
Sarika.nazare <nrsarika10@gmail.com>
2022-07-20 10:20:51 +0530

    line added

--- src/test/java/Sarika/Conflict_demo.java
+++ src/test/java/Sarika/Conflict_demo.java
@@ -2,7 +2,7 @@ package Sarika;
 
 public class Conflict_demo {
 public static void main(String[] args) {
-	System.out.println("10");
-	System.out.println("20");
+	System.out.println("100");
+	System.out.println("200");
 }
 }
