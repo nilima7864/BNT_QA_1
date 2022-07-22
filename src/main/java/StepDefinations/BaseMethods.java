06205a27437fe8e714eb63da2feccd821100f952
Ankit Dhakulkar <ankit.dhakulkar@bnt.local>
2022-07-22 18:42:24 +0530

    Ankit_AutomationFramework

--- src/main/java/StepDefinations/BaseMethods.java
+++ src/main/java/StepDefinations/BaseMethods.java
@@ -3,12 +3,13 @@ package StepDefinations;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import Constent.locators;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseMethods {
 	
-	public static WebDriver drive;
-	private static ChromeDriver driver;
+	public static WebDriver driver;
+//	private static ChromeDriver driver;
 
 	 void lunchTheBrpwser() {
 		WebDriverManager.chromedriver().setup();
